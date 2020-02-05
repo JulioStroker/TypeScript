ab251771af65a02111cff5a9cf695a0dd45c3ddc
Julio <juliorenan@live.com>
2020-02-05 20:37:17 -0300

    Inicio do projeto

---
+++ 01-alurabank/alurabank/app/js/models/Negociacao.js
@@ -0,0 +1,14 @@
+class Negociacao {
+    constructor(data, quantidade, valor){
+        
+        // conversão que diz que as propriedades não podem ser utilizadas fora da classe!
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get data (){
+    
+    }
+
+}
