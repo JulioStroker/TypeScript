482036fcffa256c24ec5af8386393dfcb66c3bc3
Julio <juliorenan@live.com>
2020-02-10 10:28:38 -0300

    finalizado primeira parte do estudo

--- 01-alurabank/alurabank/app/js/models/Negociacao.js
+++ 01-alurabank/alurabank/app/js/models/Negociacao.js
@@ -1,14 +1,19 @@
 class Negociacao {
-    constructor(data, quantidade, valor){
-        
-        // conversão que diz que as propriedades não podem ser utilizadas fora da classe!
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
     }
-
-    get data (){
-    
+    get data() {
+        return this._data;
     }
-
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
 }
